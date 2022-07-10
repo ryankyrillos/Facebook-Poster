30848e79fef160901dedab3263a0a43860f08985
ryankyrillos <32022435+ryankyrillos@users.noreply.github.com>
2022-07-10 12:47:13 +0300

    Add files via upload

---
+++ FacebookSettings.cs
@@ -0,0 +1,7 @@
+namespace Facebook_Poster
+{
+    public static class FacebookSettings
+    {
+        public static string AccessToken = "EAAKpxLZAvWEwBAGfZC0NPQum0VN0dMZB5ZAuFo6OyCLyHzN19pwbO9YQfZB5Pa0M7nxERApTNaYMECmD39nCuynpM23ZCdiiUAbr9r9rRbVDY5yjKv8wjBD9maiLkpKpFdOwfuUZBEYOLxns3WAEAudL5BmVk6NQBZCZCO5t5ZBt4WoZC3t5eMSrX6w";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Facebook_Poster
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var facebookClient = new FacebookClient();
+            var facebookService = new FacebookService(facebookClient);
+            var getAccountTask = facebookService.GetAccountAsync(FacebookSettings.AccessToken);
+            Task.WaitAll(getAccountTask);
+            var account = getAccountTask.Result;
+            Console.WriteLine($"{account.Id} {account.Name}");
+            string val;
+            Console.Write("Enter file path: ");
+                
+            var postOnWallTask = facebookService.PostOnWallAsync(FacebookSettings.AccessToken,
+            "Hello from C# .NET Core!");
+            Task.WaitAll(postOnWallTask);
+        }
+    }    
+}
